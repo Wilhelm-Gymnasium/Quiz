2964915a4e721f222e4b3ab3984091bcc8dcc1d6
wiebkeniebuhr <wiebke.niebuhr@live.de>
2015-05-21 16:31:23 +0200

    Edit categories support

--- jeopardy/src/main/java/de/wilhelmgym/jeopardy/structure/Question.java
+++ jeopardy/src/main/java/de/wilhelmgym/jeopardy/structure/Question.java
@@ -13,19 +13,20 @@ public class Question {
 
     private String question;
     private Answers answers;
+    private String category;
     private int level;
 
-    //TODO categories
     //TODO JSON parsing
 
-    public Question(String question, Answers answer, int level) {
+    public Question(String question, Answers answer, String category, int level) {
         this.question = question;
         this.answers = answer;
+        this.category = category;
         this.level = level;
     }
 
-    public Question(String question, Answers answer) {
-        this(question, answer, MEDIUM);
+    public Question(String question, Answers answer, String category) {
+        this(question, answer, category, MEDIUM);
     }
 
     public String getQuestion() {
@@ -42,6 +43,14 @@ public class Question {
 
     public void setAnswers(Answers answers) {
         this.answers = answers;
+    }
+
+    public String getCategory(String category) {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public int getLevel() {
